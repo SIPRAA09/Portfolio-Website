39d842e570a5d5c20169101d592c78d777a43ae4
SIPRAA09 <91338887+SIPRAA09@users.noreply.github.com>
2024-03-28 14:35:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            window.scrollTo({
+                top: target.offsetTop,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
